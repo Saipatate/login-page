ce4bb7b2db64914608ec442a7355cdb3503c4f25
Saipatate <halgandewen44@gmail.com>
2024-06-07 00:26:10 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector(".burger")
+const burger_menu = document.querySelector(".burger_menu")
+
+burger.addEventListener("click", () => {
+    burger_menu.classList.toggle("active")
+    burger.classList.toggle("active")
+})
